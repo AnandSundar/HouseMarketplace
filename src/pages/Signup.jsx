a5540f3eacb130456546c486331e6bec5e173ac3
AnandSundar <anandsundar96@gmail.com>
2022-02-21 15:36:57 -0600

    successfully registered user

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import {useState} from 'react'
+import React, {useState} from 'react'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
@@ -24,6 +25,27 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+        const auth = getAuth()
+
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+        const user = userCredential.user
+
+        updateProfile(auth.currentUser, {
+            displayName: name
+        })
+
+        navigate('/')
+
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
   return (
     <>
         <div className="pageContainer">
@@ -33,7 +55,7 @@ function SignUp() {
                 </p>
             </header>
 
-            <form>
+            <form onSubmit={onSubmit}>
 
                 <input type="text" className="nameInput" placeholder='Name' id='name' value={name} onChange={onChange}/>
 
