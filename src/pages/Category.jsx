d309378d7211d08af7ef79f5d48465de8ba0d68b
AnandSundar <anandsundar96@gmail.com>
2022-02-21 18:54:09 -0600

    Implemented listingItem and Offers component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,7 +4,7 @@ import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'fir
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
-
+import ListingItem from '../components/ListingItem'
 function Category() {
 
   const [listings, setListings] = useState(null)
@@ -57,7 +57,7 @@ function Category() {
                 <main>
                     <ul className="categoryListings">
                         {listings.map((listing) => {
-                            return <h3 key={listing.id}>{listing.data.name}</h3>
+                            return <ListingItem listing={listing.data} id={listing.id} key={listing.id}/>
                         })}
                     </ul>
                 </main>
